22262c11cf61b6d91cf893b98db677238fa724af
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2023-07-01 00:38:30 +0800

    防止Null值造成报错

--- elec_monitor.py
+++ elec_monitor.py
@@ -10,9 +10,9 @@ cookies = {'eai-sess': ''}
 mail_url = "https://pro-ivan.com/api/e-mail/"
 
 
-def mail(mailto, surplus, surplus_time):
+def mail(mailto, drom_Number, surplus, surplus_time):
     mail_data = {'mailto': mailto, 'subject': 'BUPT电费低警告',
-                 'body': f'截至{surplus_time}您宿舍的电费仅剩{surplus}元，已小于15元。为了防止断电给您造成损失，请及时充值！'}
+                 'body': f'截至{surplus_time}您宿舍(编号: {drom_Number})的电费仅剩{surplus}元，已小于15元。为了防止断电给您造成损失，请及时充值！'}
     mail_response = requests.post(mail_url, data=mail_data)
     print(mail_response.text)
 
@@ -22,12 +22,21 @@ def monitor(drom_Number, mailto):
     response = requests.post(base_url, data=post_data, cookies=cookies)
     # print(post_data)
     data = json.loads(response.text)
-    surplus = float(data["d"]["data"]["surplus"])
-    surplus_time = str(data["d"]["data"]["time"])
-    if surplus <= 15:
-        mail(mailto, surplus, surplus_time)
-    print(f"{drom_Number}: OK!")
-    time.sleep(1.2)
+    msg = data["m"]
+    if msg == "操作成功":
+        try:
+            surplus_time = str(data["d"]["data"]["time"])
+            surplus = float(data["d"]["data"]["surplus"])
+        except:
+            mail(mailto, drom_Number, 'Null', 'Null')
+            return True
+        if surplus <= 15:
+            mail(mailto, drom_Number, surplus, surplus_time)
+        print(f"{drom_Number}: OK!")
+        time.sleep(1.2)
+        return True
+    else:
+        return False
 
 
 def read():
@@ -39,7 +48,19 @@ def read():
     for item in data:
         dromNumber = item['dromNumber']
         mailto = item['mailto']
-        monitor(dromNumber, mailto)
+        retry = 0
+        while True:
+            try:
+                monitor(dromNumber, mailto)
+                break
+            except:
+                retry = retry +1
+                if retry == 5:
+                    jsonerr_data = {'mailto': '2531667489@qq.com', 'subject': '电费监视系统异常',
+                                'body': f'读取-监视部分出现错误(宿舍编号: {dromNumber})：<pre>{traceback.format_exc()}</pre>'}
+                    jsonerr_response = requests.post(mail_url, data=jsonerr_data)
+                    print(jsonerr_response.text)
+                    break
 
 def read_sql():
     # 连接MySQL数据库
@@ -54,16 +75,18 @@ def read_sql():
     # 遍历查询结果，打印出dromNumber和mailto的值
     for (dromNumber, mailto) in cursor:
         retry = 0
-        while True or retry < 5:
+        while True:
             try:
                 monitor(dromNumber, mailto)
                 break
             except:
-                sqlerr_data = {'mailto': '2531667489@qq.com', 'subject': '电费监视系统异常',
-                            'body': f'读取-监视部分出现错误：<pre>{traceback.format_exc()}</pre>'}
-                sqlerr_response = requests.post(mail_url, data=sqlerr_data)
-                print(sqlerr_response.text)
                 retry = retry +1
+                if retry == 5:
+                    sqlerr_data = {'mailto': '2531667489@qq.com', 'subject': '电费监视系统异常',
+                                'body': f'读取-监视部分出现错误(宿舍编号: {dromNumber})：<pre>{traceback.format_exc()}</pre>'}
+                    sqlerr_response = requests.post(mail_url, data=sqlerr_data)
+                    print(sqlerr_response.text)
+                    break
 
     # 关闭数据库连接
     cursor.close()
@@ -74,15 +97,15 @@ while True:
     now = datetime.datetime.now()
     hour = now.hour
 
-    if hour == 9 or hour == 21:
+    if hour == 15:
         # 开始一个新循环
         try:
-            read()
+            read_sql()
         except:
             err_data = {'mailto': '2531667489@qq.com', 'subject': '电费监视系统异常',
-                        'body': f'出现错误：<pre>{traceback.format_exc()}</pre>'}
+                        'body': f'总循环出现错误：<pre>{traceback.format_exc()}</pre>'}
             err_response = requests.post(mail_url, data=err_data)
             print(err_response.text)
 
     # 暂停一段时间，避免过于频繁地检测时间
-    time.sleep(1800)
+    time.sleep(3599)
